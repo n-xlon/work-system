ea6c271233eac296060af6234a939cb254a0d8a4
n-xlon <2128330672@qq.com>
2020-09-14 14:53:59 +0800

    remove console

--- vue.config.js
+++ vue.config.js
@@ -37,5 +37,10 @@ module.exports = {
         ]
       }
     }
+  },
+  configureWebpack: config => {
+    if (process.env.NODE_ENV === 'production') {
+      config.optimization.minimizer[0].options.terserOptions.compress.drop_console = true
+    }
   }
 }
