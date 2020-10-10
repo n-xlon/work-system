f6e5b0ac06d521b55d5778989d7daba0dbe3f20a
-l <min.xu@MinXudeMacBook-Pro.local>
2020-10-10 15:20:09 +0800

    fix api

--- src/store/modules/User.js
+++ src/store/modules/User.js
@@ -30,7 +30,7 @@ export default {
     login3 ({ commit }, payload) {
       return new Promise((resolve, reject) => {
         const params = location.href.match(/[?|&]code\\=(\w+)/)
-        axios.get(apis.LOGIN3, { code: params ? params[1] : '' }).then(res => {
+        axios.get(`${apis.LOGIN3}?code=${params ? params[1] : ''}`).then(res => {
           console.log(res)
           const { SessionKey, EmployeeInfo } = res
           localStorage.setItem('userInfo', JSON.stringify({ SessionKey, EmployeeInfo }))
