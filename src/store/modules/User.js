5261759c7fa9b9286e878c060ffcb37e9ea03aa9
Mr.Xu <xumin@xumindeMacBook-Pro.local>
2020-10-20 22:39:25 +0800

    fix user api

--- src/store/modules/User.js
+++ src/store/modules/User.js
@@ -14,32 +14,44 @@ export default {
     }
   },
   actions: {
-    loginIn ({ commit }, payload) {
+    // loginIn ({ commit }, payload) {
+    //   return new Promise((resolve, reject) => {
+    //     const params = location.href.match(/[?|&]code=([^&]*)[&]?/)
+    //     axios.post(apis.LOGIN, { code: '' }).then(res => {
+    //       const { SessionKey, EmployeeInfo } = res
+    //       localStorage.setItem('userInfo', JSON.stringify({ SessionKey, EmployeeInfo }))
+    //       commit('updateUserInfo', { SessionKey, EmployeeInfo })
+    //       resolve(res)
+    //     }).catch(e => {
+    //       reject(e)
+    //     })
+    //   })
+    // },
+    loginIn ({ commit, dispatch }, payload) {
       return new Promise((resolve, reject) => {
         const params = location.href.match(/[?|&]code=([^&]*)[&]?/)
-        axios.post(apis.LOGIN, { code: '' }).then(res => {
-          const { SessionKey, EmployeeInfo } = res
-          localStorage.setItem('userInfo', JSON.stringify({ SessionKey, EmployeeInfo }))
-          commit('updateUserInfo', { SessionKey, EmployeeInfo })
-          resolve(res)
+        axios.get(`${apis.LOGIN3}?code=${params ? params[1] : ''}`).then(res => {
+          console.log(res)
+          const { SessionKey, EmployeeInfo, ErrorCode } = res
+          if (ErrorCode != 0) {
+            dispatch('jumpToAuthority')
+            reject()
+          } else {
+            localStorage.setItem('userInfo', JSON.stringify({SessionKey, EmployeeInfo}))
+            commit('updateUserInfo', {SessionKey, EmployeeInfo})
+            resolve(res)
+          }
         }).catch(e => {
           reject(e)
         })
       })
     },
-    login3 ({ commit }, payload) {
-      return new Promise((resolve, reject) => {
-        const params = location.href.match(/[?|&]code=([^&]*)[&]?/)
-        axios.get(`${apis.LOGIN3}?code=${params ? params[1] : ''}`).then(res => {
-          console.log(res)
-          const { SessionKey, EmployeeInfo } = res
-          localStorage.setItem('userInfo', JSON.stringify({ SessionKey, EmployeeInfo }))
-          commit('updateUserInfo', { SessionKey, EmployeeInfo })
-          resolve(res)
-        }).catch(e => {
-          reject(e)
-        })
-      })
+    jumpToAuthority () {
+      const dom = document.createElement('a')
+      dom.href = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wwc8c054f48a782ad6&redirect_uri=www.ccodding.top%3a2000&response_type=code&scope=snsapi_base&state=STATE#wechat_redirect'
+      document.body.appendChild(dom)
+      dom.click()
+      document.body.removeChild(dom)
     },
     getUserList (_, payload) {
       return new Promise((resolve, reject) => {
