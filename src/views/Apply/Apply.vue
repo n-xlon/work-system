3aa7a6cdcd105c8784b5ed8b906b0b1a6113a6dc
-l <min.xu@MinXudeMacBook-Pro.local>
2020-11-04 17:46:47 +0800

    data

--- src/views/Apply/Apply.vue
+++ src/views/Apply/Apply.vue
@@ -8,7 +8,7 @@
           <i :class="['arrow', 'el-icon-arrow-right', {'is-open': item.isChecked}]"></i>
         </div>
         <div class="apply-details" v-if="item.isChecked">
-          <div class="list"><span>申请时间：{{item.Created.splice('.')[0]}}</span><span>申请金额（元）：{{item.Amount}}</span></div>
+          <div class="list"><span>申请时间：{{getCreatedTime}}</span><span>申请金额（元）：{{item.Amount}}</span></div>
           <div class="list"><span>审批意见：{{item.Status}}</span></div>
         </div>
       </div>
@@ -30,6 +30,11 @@ export default {
   },
   created () {
     this.getApply()
+  },
+  computed: {
+    getCreatedTime (item) {
+      return item.Created ? `${item.Created}`.splice('.')[0] : ''
+    }
   },
   mounted () {
     this.initListenEvent()
